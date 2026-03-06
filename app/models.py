c1ea2224f5f085a131cf4b87941dfa7110b5b920
plastic_lion <tongtonghoanghai@gmail.com>
2026-03-07 00:07:01 +0700

    testing

--- app/models.py
+++ app/models.py
@@ -3,14 +3,15 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index = True, unique=True)
     email: so.Mapped[str]  = so.mapped_column(sa.String(120), index = True, unique = True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='uploadder')
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,15 +22,27 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+    @login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
+
+class Author(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(30))
+    books: so.WriteOnlyMapped['Book'] = so.relationship(back_populates='author_referenced')
+
+class Book(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    title: so.Mapped[str] = so.mapped_column(sa.String(60))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='book')
+    author_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Author.id), index=True)
+    author_referenced: so.Mapped[Author] = so.relationship(back_populates='books')
+
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(300))
-    timestamp: so.Mapped[datetime] = so.mapped_column(index = True, default = lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index = True)
-
-    author: so.Mapped[User] = so.relationship(back_populates = 'posts')
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
-    
-
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    book_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Book.id), index=True)
+    uploader: so.Mapped[User] = so.relationship(back_populates='posts')
+    book: so.Mapped[Book] = so.relationship(back_populates='posts')
