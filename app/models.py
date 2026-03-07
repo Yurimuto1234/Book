3e5bed01b64fa160109d6566bd0783e0274e2aca
plastic_lion <tongtonghoanghai@gmail.com>
2026-03-07 09:58:49 +0700

    a

--- app/models.py
+++ app/models.py
@@ -8,20 +8,23 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index = True, unique=True)
-    email: so.Mapped[str]  = so.mapped_column(sa.String(120), index = True, unique = True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='uploader')
-    
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    books: so.WriteOnlyMapped['Book'] = so.relationship(back_populates='added_by_user')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
