a05b7d3a5f6d443e39e1aecaa1fb66a3c65aa07c
plastic_lion <tongtonghoanghai@gmail.com>
2026-03-08 09:19:18 +0700

    y

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,36 @@ from flask_login import logout_user, login_required
 from flask import request
 from urllib.parse import urlsplit
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = AddBookForm()
+    if form.validate_on_submit():
+        cover_data = None
+        cover_mimetype = None
+        f = form.cover.data
+        if f and f.filename:
+            cover_data = f.read()
+            cover_mimetype = f.mimetype or 'image/jpeg'
+
+        book = Book(
+            title=form.title.data,
+            author=form.author.data,
+            genre=form.genre.data or None,
+            year=form.year.data,
+            description=form.description.data or None,
+            cover_data=cover_data,
+            cover_mimetype=cover_mimetype,
+            added_by=current_user.id
+        )
+        db.session.add(book)
+        db.session.commit()
+        flash('Book added successfully!')
+        return redirect(url_for('index'))
+
     books = db.session.scalars(sa.select(Book).order_by(Book.timestamp.desc())).all()
-    return render_template('index.html', title='Book Catalog', books=books)
+    return render_template('index.html', title='Book Catalog', books=books, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -53,33 +77,33 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/add_book', methods=['GET', 'POST'])
-@login_required
-def add_book():
-    form = AddBookForm()
-    if form.validate_on_submit():
-        cover_data = None
-        cover_mimetype = None
-        f = form.cover.data
-        if f and f.filename:
-            cover_data = f.read()
-            cover_mimetype = f.mimetype or 'image/jpeg'
+# @app.route('/add_book', methods=['GET', 'POST'])
+# @login_required
+# def add_book():
+#     form = AddBookForm()
+#     if form.validate_on_submit():
+#         cover_data = None
+#         cover_mimetype = None
+#         f = form.cover.data
+#         if f and f.filename:
+#             cover_data = f.read()
+#             cover_mimetype = f.mimetype or 'image/jpeg'
 
-        book = Book(
-            title=form.title.data,
-            author=form.author.data,
-            genre=form.genre.data or None,
-            year=form.year.data,
-            description=form.description.data or None,
-            cover_data=cover_data,
-            cover_mimetype=cover_mimetype,
-            added_by=current_user.id
-        )
-        db.session.add(book)
-        db.session.commit()
-        flash('Book added successfully!')
-        return redirect(url_for('index'))
-    return render_template('add_book.html', title='Add Book', form=form)
+#         book = Book(
+#             title=form.title.data,
+#             author=form.author.data,
+#             genre=form.genre.data or None,
+#             year=form.year.data,
+#             description=form.description.data or None,
+#             cover_data=cover_data,
+#             cover_mimetype=cover_mimetype,
+#             added_by=current_user.id
+#         )
+#         db.session.add(book)
+#         db.session.commit()
+#         flash('Book added successfully!')
+#         return redirect(url_for('index'))
+#     return render_template('add_book.html', title='Add Book', form=form)
 
 @app.route('/book/<int:book_id>/cover')
 @login_required
